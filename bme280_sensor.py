da937197929ca8aa49ebc654145cf52ec5121f1c
Guido <makuto@hotmail.it>
2020-04-17 21:49:10 +0200

    Second commit

--- bme280_sensor.py
+++ bme280_sensor.py
@@ -3,9 +3,10 @@ import smbus2
 import cv2
 from time import sleep
 import numpy as np
+import time
 
 
-def sample_data_bme280(window):
+def sample_data_bme280(window, cursor, connection):
     port = 1
     address = 0x76
     bus = smbus2.SMBus(port)
@@ -17,6 +18,15 @@ def sample_data_bme280(window):
         humidity = bme280_data.humidity
         pressure = bme280_data.pressure
         ambient_temperature = bme280_data.temperature
+
         print("H: {} P: {} T: {}".format(humidity, pressure, ambient_temperature))
+
         window.lcd_h.display(humidity)
-        sleep(1)
+        window.lcd_p.display(pressure)
+        window.lcd_t.display(ambient_temperature)
+
+        date_now = time.strftime('%Y-%m-%d %H:%M:%S')
+        cursor.execute("INSERT INTO BME_280 VALUES (%s, %s, %s, %s)", (ambient_temperature, pressure, humidity, date_now))
+        connection.commit()
+
+        sleep(2)
--- main.py
+++ main.py
@@ -2,32 +2,46 @@ from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5 import uic, QtCore
 import sys
-from bme280_test import sample_data_bme280
+from bme280_sensor import sample_data_bme280
+import mysql.connector as mariadb
+import time
+
 
 # Ui_MainWindow, QtBaseClass = uic.loadUiType("untitled.ui")
 
 
 class ThreadAnalysis(QtCore.QThread):
 
-    def __init__(self, window):
+    def __init__(self, window, cursor, connection):
         QtCore.QThread.__init__(self)
         self.window = window
+        self.cursor = cursor
+        self.connection = connection
 
     def __del__(self):
         self.wait()
 
     def run(self):
-        sample_data_bme280(self.window)
+        sample_data_bme280(self.window, self.cursor, self.connection)
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, *args, **kwargs):
+    def __init__ (self, *args, **kwargs):
 
         super().__init__(*args, **kwargs)
         uic.loadUi("main.ui", self)
 
+        # database
+        self.mariadb_connection = mariadb.connect(user='pi', password='dardinello', database='home_station')
+        self.cursor = self.mariadb_connection.cursor()
+
+        # self.cursor.execute("SELECT * FROM BME_280")
+        # records = self.cursor.fetchall()
+        # for r in records:
+        #     print(r)
+
         # threads
-        self.sampling_thread = ThreadAnalysis(self)
+        self.sampling_thread = ThreadAnalysis(self, self.cursor, self.mariadb_connection)
 
         # buttons
         self.button_start.clicked.connect(self.start_sampling)
